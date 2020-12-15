f44de720a65f8221dd54479bca705509398aa3d9
Chase Zhou <czhou@mitre.org>
2020-12-15 10:58:47 -0500

    adding test for footer

--- src/components/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Footer from '../Footer';
-
-test('renders Inferno Footer', () => {
-  render(<Footer />);
-  const linkElement = screen.getByText(/Open Source/i);
-  expect(linkElement).toBeInTheDocument();
-});
