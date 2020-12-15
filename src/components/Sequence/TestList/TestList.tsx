6a73eb73a99a4cd40b4dc7117b5bb9ee6e8d5fa8
Chase Zhou <czhou@mitre.org>
2020-12-15 09:25:30 -0500

    initial commit adding sequence components

---
+++ src/components/Sequence/TestList/TestList.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import useStyles from '../styles';
+import { ListItemIcon, ListItemText } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { green, red } from '@material-ui/core/colors';
+
+export interface TestListProps {
+  tests: Test[];
+}
+
+export interface Test {
+  description: string;
+  result: TestResult;
+}
+
+export enum TestResult {
+  Success,
+  Failure,
+  Skipped,
+  None,
+}
+
+const TestList: FC<TestListProps> = ({ tests }) => {
+  const styles = useStyles();
+  return (
+    <List className={styles.root}>
+      {tests.map((test) => (
+        <ListItem>
+          <ListItemIcon>
+            {test.result === TestResult.Success ? (
+              <CheckIcon style={{ color: green[500] }} />
+            ) : (
+              <CancelIcon style={{ color: red[500] }} />
+            )}
+          </ListItemIcon>
+          <ListItemText primary={test.description} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default TestList;
